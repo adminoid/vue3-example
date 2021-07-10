7b9b3612d6ef157c33e0dafe29a05d452cf14242
Petr Zenin <mr@adminoid.com>
2021-07-10 07:38:22 +0300

    custom variables and fonts for all weight options

--- src/ts/layouts/Dashboard.vue
+++ src/ts/layouts/Dashboard.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
-h1 Dashboard layout
+.container
+  h1.text-success Dashboard layout
+  p.black.text-success Dashboard layout
+  p.soft-black Dashboard layout
+  p.bold Dashboard layout
+  p.medium Dashboard layout
+  p.light Dashboard layout
 </template>
 
 <script lang="ts">
@@ -12,5 +18,15 @@ export default defineComponent({
 
 <style lang="sass" scoped>
 h1
-  font-size: 11em
+  font-size: 3em
+p.black
+  font-weight: 900
+p.soft-black
+  font-weight: 800
+p.bold
+  font-weight: bold
+p.medium
+  font-weight: 500
+p.light
+  font-weight: 300
 </style>
