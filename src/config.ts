97ec6000127e23c21630aca28db08ef8ed9436d5
Petr Zenin <mr@adminoid.com>
2021-07-12 16:03:50 +0300

    visual show/hide sidebar

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const leftSidebarWidth = '300px'
--- src/ts/components/LeftSidebar.vue
+++ src/ts/components/LeftSidebar.vue
@@ -1,13 +1,37 @@
 <template lang="pug">
-.left-sidebar Sidebar
+.left-sidebar(:style="[{width}, styles]") {{ width }}
 </template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
+import { leftSidebarWidth as width } from '@/config'
+
+const LeftSidebar = defineComponent({
+  setup () {
+    const store = useStore()
+    const sidebarOpen = computed(_ => store.state.layout.sidebar.open)
+    const styles = computed(_ => {
+      if (!sidebarOpen.value) {
+        return {
+          left: `-${width}`
+        }
+      }
+      return false
+    })
+    return {
+      width,
+      styles
+    }
+  }
+})
+
+export default LeftSidebar
+</script>
 
 <style lang="sass">
 .left-sidebar
-  display: none
   position: fixed
-  //width: $left-sidebar-width
   background: #c5dce5
   min-height: 100%
-  top: 0
 </style>
