ee103b2db8c32e32cc5890c4445652183a6fec0c
Petr Zenin <mr@adminoid.com>
2021-07-20 05:01:08 +0300

    dynamic components in MainContent

--- src/ts/components/MainContent.vue
+++ src/ts/components/MainContent.vue
@@ -1,27 +1,39 @@
 <template lang="pug">
 main(:class="{ padding: hasPadding }")
   .container-fluid
-    | Page name is:
-    strong {{ page }}
+    component(:is="pageComponent")
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
+import SelectObjectExample from 'c@/pages/SelectObjectExample.vue'
+import SelectTimeExample from 'c@/pages/SelectTimeExample.vue'
+import { pascalCase } from 'change-case'
 
 const MainContent = defineComponent({
+  components: {
+    SelectObjectExample,
+    SelectTimeExample
+  },
+
   setup () {
     const store = useStore()
     const route = useRoute()
-    console.log(route.params.page)
-    const page = computed(_ => route.params.page)
+    const pageComponent = computed(_ => {
+      let componentName: string | boolean = false
+      if (route.params.page) {
+        componentName = pascalCase(String(route.params.page))
+      }
+      return componentName
+    })
     const hasPadding = computed(
       _ => store.state.layout.sidebar.open &&
         store.state.layout.sidebar.pinned)
     return {
       hasPadding,
-      page
+      pageComponent
     }
   }
 })
@@ -35,6 +47,7 @@ main
   float: right
   width: 100%
   transition: width $left-sidebar-animation-speed ease-in-out
+  padding: 32px 20px
   &.padding
     width: calc(100% - #{$left-sidebar-width})
 </style>
---
+++ src/ts/components/pages/SelectObjectExample.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+h1 Пример выборки объектов
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const SelectObjectExample = defineComponent({})
+
+export default SelectObjectExample
+</script>
