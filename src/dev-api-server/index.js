be04a61d20cf22543a3c17e58a332481c2a4ff20
Petr Zenin <mr@adminoid.com>
2021-07-27 17:47:08 +0300

    added dotenv to dev api server

--- src/dev-api-server/index.js
+++ src/dev-api-server/index.js
@@ -3,10 +3,11 @@ const cors = require('cors')
 const app = express()
 const pages = require('./pages')
 
+require('dotenv').config()
 app.use(cors())
 
 app.get('/api/pages/:page', function (req, res) {
   res.json(pages[req.params.page])
 })
 
-app.listen(8081, () => console.info('CORS-enabled dev api server listening on port 8081'))
+app.listen(process.env.API_PORT, () => console.info('CORS-enabled dev api server listening on port 8081'))
