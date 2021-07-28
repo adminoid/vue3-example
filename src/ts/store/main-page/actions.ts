c2fd3e49e1a2c5a895550be0c3d38d4c1dc62bab
Petr Zenin <mr@adminoid.com>
2021-07-28 17:14:54 +0300

    temp local json backend for laradock compatibility

--- src/ts/store/main-page/actions.ts
+++ src/ts/store/main-page/actions.ts
@@ -1,9 +1,19 @@
 import axios from 'axios'
-const apiHost = process.env.VUE_APP_API_HOST
+
+console.log(process.env.VUE_APP_LOCAL, typeof process.env.VUE_APP_LOCAL)
+
+let mpUrl : string // main page data url
+if (process.env.VUE_APP_LOCAL === 'true') {
+  mpUrl = `${process.env.VUE_APP_API_HOST}/api/pages/main-page`
+} else {
+  mpUrl = '/api/main-page.json'
+}
+
+console.log(mpUrl)
 
 export default {
   async getPageData ({ commit }: { commit: Function }) {
-    const response = await axios.get(`${apiHost}/api/pages/main-page`)
+    const response = await axios.get(mpUrl)
     commit('setPageData', response.data)
   }
 }
