d76a5fdcb269a079cc374c0a5ad3770fc452a591
Petr Zenin <mr@adminoid.com>
2021-07-30 13:01:34 +0300

    added id to main menu items

--- src/ts/components/MainMenu.vue
+++ src/ts/components/MainMenu.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 ul.main-menu(v-if="items.length > 0")
-  li.main-menu__fi.mb-1(v-for="item in items")
-    a.main-menu__expand(href='#' @click.prevent)
+  li.main-menu__fi.mb-1(v-for="item in items" :key="item.id")
+    a.main-menu__expand(href='#' @click.prevent="expand")
       svg(width='12px' height='12px' xmlns='http://www.w3.org/2000/svg')
-        g#expand(fill='#000000')
+        g#expand(fill='#000')
           rect#Rectangle(x='0' y='5' width='12' height='2' rx='1')
           rect#Rectangle(x='5' y='0' width='2' height='12' rx='1')
     router-link(:to="item.uri") {{ item.title }}
     ul.main-menu__submenu.list-unstyled(v-if="item.children?.length > 0")
-      li.main-menu__si.mb-1(v-for="subItem in item.children")
+      li.main-menu__si.mb-1(v-for="subItem in item.children" :key="item.id")
         router-link(:to="subItem.uri") {{ subItem.title }}
 </template>
 
@@ -18,6 +18,11 @@ import { items, TItems } from '@/ts/includes/main-menu'
 
 const MainMenu = defineComponent({
   setup () {
+    // const
+    // const expand = () => {
+    //
+    // }
+
     return {
       items
     } as {items: TItems}
--- src/ts/includes/main-menu.ts
+++ src/ts/includes/main-menu.ts
@@ -1,7 +1,9 @@
 export type TItems = {
+  id: number,
   uri: string,
   title: string,
   children?: {
+    id: number,
     uri: string,
     title: string
   }[]
@@ -9,140 +11,172 @@ export type TItems = {
 
 export const items = [
   {
+    id: 2,
     uri: '/metrics',
     title: 'Метрики'
   },
   {
+    id: 3,
     uri: '/monitoring',
     title: 'Мониторинг'
   },
   {
+    id: 4,
     uri: '/settings',
     title: 'Настройки',
     children: [
       {
+        id: 5,
         uri: '/settings/program-cost',
         title: 'Стоимость программ'
       },
       {
+        id: 6,
         uri: '/settings/chem-dosage',
         title: 'Дозация химии'
       },
       {
+        id: 7,
         uri: '/settings/pressure',
         title: 'Настройка давления'
       },
       {
+        id: 8,
         uri: '/settings/posts',
         title: 'Посты и пылесосы'
       },
       {
+        id: 9,
         uri: '/settings/one-time',
         title: 'Одноразовые настройки'
       },
       {
+        id: 10,
         uri: '/settings/customers',
         title: 'Пользователи'
       },
       {
+        id: 11,
         uri: '/settings/cash',
         title: 'Онлайн касса'
       },
       {
+        id: 12,
         uri: '/settings/options',
         title: 'Настройки опций объекта'
       },
       {
+        id: 13,
         uri: '/settings/description',
         title: 'Описание объекта'
       }
     ]
   },
   {
+    id: 14,
     uri: '/statistic',
     title: 'Статистика',
     children: [
       {
+        id: 15,
         uri: '/statistic/common',
         title: 'Статистика общая'
       },
       {
+        id: 16,
         uri: '/statistic/hourly',
         title: 'График по часам'
       },
       {
+        id: 17,
         uri: '/statistic/daily',
         title: 'График по дням'
       },
       {
+        id: 18,
         uri: '/statistic/programs',
         title: 'Использование программ'
       },
       {
+        id: 19,
         uri: '/statistic/avg-check',
         title: 'Средний чек'
       }
     ]
   },
   {
+    id: 20,
     uri: '/finance',
     title: 'Финансы',
     children: [
       {
+        id: 21,
         uri: '/finance/cash',
         title: 'Деньги в кассе'
       },
       {
+        id: 22,
         uri: '/finance/collection-history',
         title: 'История инкассаций'
       }
     ]
   },
   {
+    id: 23,
     uri: '/bonus',
     title: 'ALLES Bonus'
   },
   {
+    id: 24,
     uri: '/accounting',
     title: 'Учет',
     children: [
       {
+        id: 25,
         uri: '/accounting/consumables',
         title: 'Учет расходников и коммуникаций'
       },
       {
+        id: 26,
         uri: '/accounting/properties',
         title: 'Хозяйственный учет'
       },
       {
+        id: 27,
         uri: '/accounting/tax-salary',
         title: 'Налоги и зарплата'
       },
       {
+        id: 28,
         uri: '/accounting/object-logs',
         title: 'Журнал объекта'
       },
       {
+        id: 29,
         uri: '/accounting/service',
         title: 'Сервисный учет объекта'
       }
     ]
   },
   {
+    id: 30,
     uri: '/economics',
     title: 'Экономика',
     children: [
       {
+        id: 31,
         uri: '/economics/profitability',
         title: 'Доходность'
       },
       {
+        id: 32,
         uri: '/economics/payback',
         title: 'Окупаемость мойки'
       }
     ]
   },
   {
+    id: 33,
     uri: '/analytics',
     title: 'Анализ мойки'
   }
