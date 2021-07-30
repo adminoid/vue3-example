32065163700728bb9fa4f1e13b2f906449a84f84
Petr Zenin <mr@adminoid.com>
2021-07-30 16:18:14 +0300

    isExpanded method added

--- src/ts/components/MainMenu.vue
+++ src/ts/components/MainMenu.vue
@@ -3,12 +3,15 @@ ul.main-menu(v-if="items.length > 0")
   li.main-menu__fi.mb-1(v-for="item in items" :key="item.id")
     a.main-menu__expand(href='#' @click.prevent="expand(item.id)")
       svg(width='12px' height='12px' xmlns='http://www.w3.org/2000/svg')
-        g#expand(fill='#000')
-          rect#Rectangle(x='0' y='5' width='12' height='2' rx='1')
-          rect#Rectangle(x='5' y='0' width='2' height='12' rx='1')
+        g#expand(
+          fill='#000'
+          :class="{ expanded: isExpanded(item) }"
+        )
+          rect#horizontal(x='0' y='5' width='12' height='2' rx='1')
+          rect#vertical(x='5' y='0' width='2' height='12' rx='1')
     router-link(:to="item.uri") {{ item.title }}
     transition(name="expand")
-      ul.main-menu__submenu.list-unstyled(v-if="item.children?.length > 0 && expandedRef.includes(item.id)")
+      ul.main-menu__submenu.list-unstyled(v-if="isExpanded(item)")
         li.main-menu__si.mb-1(v-for="subItem in item.children" :key="item.id")
           router-link(:to="subItem.uri") {{ subItem.title }}
 </template>
@@ -27,11 +30,15 @@ const MainMenu = defineComponent({
         if (!expandedRef.value.includes(id)) expandedRef.value.push(id)
       }
     }
+    const isExpanded = (item: any) => {
+      return item.children?.length > 0 && expandedRef.value.includes(item.id)
+    }
 
     return {
       items,
       expand,
-      expandedRef
+      expandedRef,
+      isExpanded
     }
   }
 })
