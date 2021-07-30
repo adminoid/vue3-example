ba355111f72430f8d149c074734b68350a7cb166
Petr Zenin <mr@adminoid.com>
2021-07-30 11:39:35 +0300

    expand icon added

--- src/ts/components/MainMenu.vue
+++ src/ts/components/MainMenu.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
-ul.main-menu.list-unstyled(v-if="items.length > 0")
-  li.mb-1(v-for="item in items")
+ul.main-menu(v-if="items.length > 0")
+  li.main-menu__fi.mb-1(v-for="item in items")
+    a.main-menu__expand(href='#' @click.prevent)
+      svg(width='12px' height='12px' xmlns='http://www.w3.org/2000/svg')
+        g#expand(fill='#000000')
+          rect#Rectangle(x='0' y='5' width='12' height='2' rx='1')
+          rect#Rectangle(x='5' y='0' width='2' height='12' rx='1')
     router-link(:to="item.uri") {{ item.title }}
     ul.main-menu__submenu.list-unstyled(v-if="item.children?.length > 0")
-      li.mb-1(v-for="subItem in item.children")
+      li.main-menu__si.mb-1(v-for="subItem in item.children")
         router-link(:to="subItem.uri") {{ subItem.title }}
 </template>
 
@@ -25,12 +30,22 @@ export default MainMenu
 @import "src/sass/variables"
 
 .main-menu
-  > li a
-    color: black
-    &:hover
-      color: darken($bb-blue, 30%)
+  padding-left: 0
+  list-style: none
+  .main-menu__fi // main menu first level item
+    position: relative
+    a
+      color: black
+      &:hover
+        color: darken($bb-blue, 30%)
+  .main-menu__si // main menu second level item
+    a
+      font-size: 15px
   .main-menu__submenu
     padding: 4px 0 1px 8px
-    > li a
-      font-size: 15px
+  .main-menu__expand
+    display: block
+    position: absolute
+    left: -17px
+    top: -1px
 </style>
