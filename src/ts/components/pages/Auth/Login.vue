918526550a8d44fc0a7edb8167d0910090a3398a
Petr Zenin <mr@adminoid.com>
2021-08-06 23:14:26 +0300

    login layout

--- src/ts/components/pages/Auth/Login.vue
+++ src/ts/components/pages/Auth/Login.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
-h1 Login
+.container.d-flex.justify-content-center.h-100.align-items-center
+  .login.d-flex.flex-column.justify-content-center.align-items-center.col-7.rounded-3.h-50
+      h1 Войдите
+      .form-floating.mb-3
+        input#floatingLogin.form-control(type='email' placeholder='name@example.com')
+        label(for='floatingLogin') Логин
+      .form-floating.mb-3
+        input#floatingPassword.form-control(type='password' placeholder='Password')
+        label(for='floatingPassword') Пароль
+      button.btn.btn-primary.btn-lg(type='submit') Войти
 </template>
 
 <script lang="ts">
@@ -12,6 +21,7 @@ const Login = defineComponent({
 export default Login
 </script>
 
-<style lang="sass">
-
+<style lang="sass" scoped>
+.login
+  background: #fff
 </style>
