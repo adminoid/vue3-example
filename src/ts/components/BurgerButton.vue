1dfd496797cf646846799263a0affc10039be6d9
Petr Zenin <mr@adminoid.com>
2021-08-12 14:20:05 +0300

    fix main content offset

--- src/ts/components/BurgerButton.vue
+++ src/ts/components/BurgerButton.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-a.burger(href='#' @click="toggle")
+a.burger(href='#' @click.prevent="toggle")
   svg.burger__sidebar-open(
     width='45px' height='32px'
     viewBox='0 0 45 32'
--- src/ts/components/MainContent.vue
+++ src/ts/components/MainContent.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-main(:class="{ padding: hasPadding }")
+main.main-content(:class="classes")
   .container-fluid
     router-view
 </template>
@@ -14,12 +14,13 @@ import { useStore } from 'vuex'
 const MainContent = defineComponent({
   setup: function () {
     const store = useStore()
-    const hasPadding = computed(
-      _ => store.state.layout.sidebar.open &&
-        store.state.layout.sidebar.pinned
-    )
+    const classes = computed(() => ({
+      'main-content_padding': store.state.layout.sidebar.open &&
+        store.state.layout.sidebar.pinned,
+      'main-content_with-pin': store.state.layout.sidebar.pinned
+    }))
     return {
-      hasPadding
+      classes
     }
   }
 })
