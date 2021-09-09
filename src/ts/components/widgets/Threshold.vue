54ab1ea71984a3f928a6deeb6a2132dd03faf7fb
Petr Zenin <mr@adminoid.com>
2021-09-09 18:15:47 +0300

    threshold component blank

--- src/ts/components/widgets/Threshold.vue
+++ src/ts/components/widgets/Threshold.vue
@@ -1,13 +1,44 @@
 <template lang="pug">
-p T:{{ data }}
+.widgets-threshold
+  span.widgets-threshold__value {{ data.value }}
+  span.widgets-threshold__changed(:style="{color: isFall ? '#D95757' : '#198754'}")
+    | {{ changedValue }}
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
+
+type TThreshold = {
+  value: number,
+  changed: number
+}
 
 export default defineComponent({
   props: {
-    data: Object as PropType<any>
+    data: {
+      type: Object as PropType<TThreshold | undefined>
+    }
+  },
+
+  setup (props) {
+    // todo: move styling to sass
+
+    const isFall = computed(() => props.data && props.data?.changed < 0)
+
+    return {
+      isFall,
+      changedValue: isFall.value ? props.data?.changed : `+${props.data?.changed}`
+    }
   }
 })
 </script>
+
+<style lang="sass">
+.widgets-threshold
+  .widgets-threshold__value
+    font-size: 12px
+  .widgets-threshold__changed
+    position: relative
+    font-size: 7px
+    top: -12px
+</style>
